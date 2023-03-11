f543e5a88ee3093e7496661f261062c64ffbb857
Jayanth <jay.codmik.18@gmail.com>
2023-03-11 20:54:04 +0530

    PinCode Checker

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,10 +1,28 @@
 import { useRouter } from 'next/router'
+import 'bootstrap/dist/css/bootstrap.css'
+import { useState } from 'react';
+
 
 const Post = ({addToCart}) => {
+
+
+
   const router = useRouter()
   const { slug } = router.query
+  const [pin, setpin] = useState()
+  const [service, setservice] = useState()
+  const checkServiceablity =async()=>{
+    let pins = await fetch('http://localhost:3000/api/pincode')
+    let pinJson =await pins.json()
+    if(pinJson.includes(parseInt(pin))){setservice(true)}
+    else{setservice(false)}
+     }
+     const onChangePin=(e)=>{
+setpin(e.target.value)
+     }
 
-  return <> <section className="text-gray-600 body-font overflow-hidden">
+  return <>
+   <section className="text-gray-600 body-font overflow-hidden">
   <div className="container px-5 py-24 mx-auto">
     <div className="lg:w-4/5 mx-auto flex flex-wrap">
       <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://assets.ajio.com/medias/sys_master/root/20201020/gMeX/5f8ef533f997dd8c836f7c0b/-473Wx593H-461465607-navy-MODEL.jpg"/>
@@ -81,10 +99,32 @@ const Post = ({addToCart}) => {
               <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
             </svg>
           </button>
-        </div>
+          
+     </div>
+
+
+
+        <div className="pincode d-flex justify-content-center">
+
+        
+        <form className='w-80 '>
+ 
+  <div class="mb-3  ">
+    <h3 for="exampleInputPassword1" class="form-label ">PinCode : </h3>
+    <input type="value" onChange={onChangePin} class="form-control" id="exampleInputPassword1"/>
+  </div>
+ 
+  <a type="submit" onClick={checkServiceablity} class="btn btn-primary">Check of Avaliablity</a>
+  {(!service&&  service!=null )&& <div className="text-red-700 sm mt-3"> We don't Deliver to this pincode  </div>}
+  {(service&&  service!=null) &&<div className="text-green-700 sm mt-3"> Serviciable  </div>}
+  
+</form>
+</div>
       </div>
     </div>
   </div>
+
+  
 </section>
 </>
 }
